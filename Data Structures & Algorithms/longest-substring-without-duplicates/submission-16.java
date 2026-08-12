169cf9fa3c5242eea03fcd6ef6643cd553b6cbb5
Kobe <kobeb@uw.edu>
2026-08-11 23:11:50 -0700

    Add: longest-substring-without-duplicates - submission-16

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-16.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> seen = new HashSet<>();
+        int left = 0;
+        int best = 0;
+        for(int right = 0; right < s.length(); right++){
+            while(seen.contains(s.charAt(right))){
+                seen.remove(s.charAt(left));
+                left++;
+            }
+            seen.add(s.charAt(right));
+            best = Math.max(right - left + 1, best);
+        }
+
+        return best;
+    }
+}
